d8a103e893ba34b491c11ed9a6da5127e54d7daf
Muhammad Salam <114971278+dev-salam@users.noreply.github.com>
2024-07-29 17:44:03 +0500

    Testimonial and Featured Products

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
 import { ProductsComponent } from '../../Components/products/products.component';
 import { HomeGalleryComponent } from '../../Components/home-gallery/home-gallery.component';
+import { TestimonialsComponent } from '../../Components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ProductsComponent, HomeGalleryComponent],
+  imports: [HeaderComponent, ProductsComponent, HomeGalleryComponent, TestimonialsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
